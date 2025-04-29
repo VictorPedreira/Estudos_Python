e776e9211abcce7583ca1a3a01b383d36bb0aa0d
VictorPedreira <fpedreiraoficial@gmail.com>
2025-04-29 18:23:12 -0300

    atu

---
+++ Aulas/aula82.py
@@ -0,0 +1,21 @@
+# Sets - Conjuntos em Python (tipo set)
+# Conjuntos s'ao ensinados na matemática
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor inteiro.
+
+# Criando um set 
+# set(iterável) ou {1, 2, 3}
+# s1 = set('Luiz')
+s1 = set() # vazio
+s1 = {'Luiz', 1, 2, 3} # com dados
+
+# Sets são eficientes para remover valores duplicados 
+# de iteráveis.
+# - eles não tem índexes;
+# - eles não garantem ordem;
+# - eles são iteráveis (for, in, not inw)
+
+# Métodos úteis:
+# add, update, clear, discard
+
