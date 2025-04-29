068494e4bd042ac31035c737825641cd3ccbcfff
VictorPedreira <fpedreiraoficial@gmail.com>
2025-04-29 18:40:14 -0300

    atualização

--- Aulas/aula82.py
+++ Aulas/aula82.py
@@ -7,15 +7,27 @@
 # Criando um set 
 # set(iterável) ou {1, 2, 3}
 # s1 = set('Luiz')
-s1 = set() # vazio
-s1 = {'Luiz', 1, 2, 3} # com dados
+# s1 = set() # vazio
+# s1 = {'Luiz', 1, 2, 3} # com dados
 
 # Sets são eficientes para remover valores duplicados 
 # de iteráveis.
 # - eles não tem índexes;
 # - eles não garantem ordem;
 # - eles são iteráveis (for, in, not inw)
+# l1 = [1, 2, 3, 3, 3, 3, 3, 1]
+# s1 = set(l1)
+# l2 = list(s1)
+s1 = {1, 2, 3}
+# print(3 not in s1)
+#for numero in s1:
+#   print(numero)
 
 # Métodos úteis:
 # add, update, clear, discard
 
+# Operadores úteis:
+# união | união (union) - Une
+# intersecções & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda 
+# diferença simétrica ^ - Itens que não estão em ambos
